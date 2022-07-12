6b2ce6547a291ab36fc4d338feb1cb6126601504
Omer Faruk Sandikci <sandikci_omer@hotmail.com>
2022-07-12 15:21:10 +0300

    Add project files.

---
+++ Level_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Level_1.Entities;
+using Level_1.ValidationRules;
+using System;
+
+namespace Level_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IValidator<Customer> validator = new CustomerValidator();
+            Customer customer = new Customer();
+
+            Console.Write("İsminizi giriniz : ");
+            customer.Name = Console.ReadLine();
+
+            Console.Write("Soyisminizi giriniz : ");
+            customer.Surname = Console.ReadLine();
+
+            Console.Write("Kimlik numaranızı giriniz : ");
+            customer.NationalityId = Console.ReadLine();
+
+            Console.Write("Yaşadığınız şehiri giriniz : ");
+            customer.City = Console.ReadLine();
+
+            var validationResult=validator.Validate(customer);
+            if (!validationResult.IsValid)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("-----------------------Hata----------------------");
+                foreach (var error in validationResult.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+            Console.Read();
+            
+        }
+    }
+}
---
+++ Level_1/ValidationRules/CustomerValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Level_1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_1.ValidationRules
+{
+    public class CustomerValidator:AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Bu alan boş geçilemez")
+                .MinimumLength(3).MaximumLength(15).WithMessage("İsminiz 3-15 karakter uzunluğunda olmalı");
+
+            RuleFor(x => x.Surname)
+                .NotEmpty().WithMessage("Bu alan boş geçilemez")
+                .MinimumLength(2).MaximumLength(15).WithMessage("Soyisminiz 2-15 karakter uzunluğunda olmalı");
+
+            RuleFor(x => x.NationalityId)
+                .NotEmpty().WithMessage("Bu alan boş geçilemez")
+                .MinimumLength(11).MaximumLength(11).WithMessage("Kimlik numaranız 11 karakter uzunluğunda olmalı");
+
+            RuleFor(x => x.City)
+                .NotEmpty().WithMessage("Bu alan boş geçilemez")
+                .MinimumLength(2).MaximumLength(13).WithMessage("Şehir ismi 2-13 karakter uzunluğunda olmalı");
+        }
+    }
+}
